2f075c8f2a14d7b2558f6237fd947049da84eb14
IH8U <grzesiek.duda06@gmail.com>
2023-03-31 13:28:37 +0200

    old Price new Price and href logging

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const findSales = () => {
+const findSales = (sale) => {
   setTimeout(() => {
     const articles = document.querySelectorAll(
       ".Articlestyles__ArticleWrapper-sc-hib3gs-0"
@@ -9,8 +9,21 @@ const findSales = () => {
         .children[1].firstChild.innerHTML.slice(0, -11);
       let newPrice = article.querySelector(
         ".Pricestyles__PriceWrapper-sc-ma110l-0"
-      ).children[3].firstChild.innerText;
-      console.log(`STARA CENA ${oldPrice} NOWA CENA ${newPrice}`);
+      );
+      newPrice =
+        newPrice.children[3].firstChild.innerText == "od"
+          ? newPrice.children[4]
+          : newPrice.children[3];
+
+      newPrice = newPrice.firstChild.innerText.slice(0, -8);
+
+      let href =
+        "https://www.zalando-lounge.pl" +
+        article.querySelector("a").getAttribute("href");
+
+      console.log(
+        `STARA CENA ${oldPrice} \nNOWA CENA ${newPrice} \nhref ${href}`
+      );
     }
   }, 3000);
 };
@@ -19,7 +32,7 @@ const addWindow = () => {
   const window = document.createElement("div");
   window.classList.add("window-botlando");
   window.innerHTML =
-    "<h2>Search best sales!</h2><button>start searching</button>";
+    '<h2>Search best sales!</h2><button id="70">Sales > 70%</button><button id="60">Sales > 60%</button><button id="50">Sales > 50%</button><h3>BotLandoLounge | <a href="https://github.com/ih8u0/BotLandoLounge" target="_blank">GitHub</a></h3>';
   document.body.appendChild(window);
   return window;
 };
