df16551cff5fcec84a972676fbd9d9cfaf766d2f
IH8U <grzesiek.duda06@gmail.com>
2023-03-31 09:54:31 +0200

    downloading dom elements

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 const findSales = () => {
-  // const articles = document.querySelector("ul#articleListWrapper");
-  const articles = document.querySelector("#articleListWrapper");
-  articlesArray = articles.getElementsByTagName("li");
-  console.log(articlesArray);
+  setTimeout(() => {
+    const articles = document.querySelectorAll(
+      ".Articlestyles__ArticleWrapper-sc-hib3gs-0"
+    );
+    for (let article of articles) {
+      let oldPrice = article
+        .querySelector(".Pricestyles__PriceWrapper-sc-ma110l-0")
+        .children[1].firstChild.innerHTML.slice(0, -11);
+      let newPrice = article.querySelector(
+        ".Pricestyles__PriceWrapper-sc-ma110l-0"
+      ).children[3].firstChild.innerText;
+      console.log(`STARA CENA ${oldPrice} NOWA CENA ${newPrice}`);
+    }
+  }, 3000);
 };
 
 const addWindow = () => {
